81e2d995dd73ee4bf30ecb6e232a27f7f822e01c
Your Name <you@example.com>
2019-03-19 16:37:54 +0800

    merge

--- app/src/main/java/com/example/linearlayouttest/MainActivity.java
+++ app/src/main/java/com/example/linearlayouttest/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.linearlayouttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Lifecycle";
+    private Button exp1;
+    private Button exp2;
+    private Button exp3;
+
+    private void showMsg(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.table_layout);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+//        showMsg("onCreate");
+        exp1 = findViewById(R.id.exp1);
+        exp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent = intent.setClass(MainActivity.this, LinearLayout.class);
+
+                startActivity(intent);
+
+            }
+        });
+        exp2 = findViewById(R.id.exp2);
+        exp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent = intent.setClass(MainActivity.this, RelativeLayout.class);
+
+                startActivity(intent);
+
+            }
+        });
+        exp3 = findViewById(R.id.exp3);
+        exp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent = intent.setClass(MainActivity.this, TableLayout.class);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+//        showMsg("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+//        showMsg("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+//        showMsg("onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+//        showMsg("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState");
+//        showMsg("onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+//        showMsg("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+//        showMsg("onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+//        showMsg("onRestart");
+
     }
 }
